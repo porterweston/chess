ad5616f4f776e02f39ac22c43bfa34c729aadf6b
Porter Weston <pweston4@byu.edu>
2024-01-22 15:12:32 -0700

    queen move tests passed

--- shared/src/main/java/chess/QueenMovesCalculator.java
+++ shared/src/main/java/chess/QueenMovesCalculator.java
@@ -1,9 +1,14 @@
 package chess;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
+import java.util.ArrayList;
 
 public class QueenMovesCalculator {
     Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position){
-        return null;
+        Collection<ChessMove> diagonalMoves = new BishopMovesCalculator().pieceMoves(board, position);
+        Collection<ChessMove> orthogonalMoves = new RookMovesCalculator().pieceMoves(board, position);
+        diagonalMoves.addAll(orthogonalMoves);
+        return diagonalMoves;
     }
 }
