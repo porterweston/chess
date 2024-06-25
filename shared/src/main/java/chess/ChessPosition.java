349e9b94b9d51bd24d47a0f93e4eead506e27311
Porter Weston <pweston4@byu.edu>
2024-06-24 19:06:50 -0600

    init(phase0)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -8,7 +8,11 @@ package chess;
  */
 public class ChessPosition {
 
+    private int row;
+    private int col;
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
