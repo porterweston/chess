83f6355ba6e6ac1a8eb384cfd3999b28447548af
porterweston <porterwestonbiz@gmail.com>
2024-08-05 16:06:41 -0600

    all DAOs implemented and tests written and passed

--- server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/mysql/MySQLGameDAO.java
@@ -1,10 +1,14 @@
 package dataaccess.mysql;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import dataaccess.*;
 import dataaccess.interfaces.*;
 import model.*;
 
-import javax.xml.crypto.Data;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class MySQLGameDAO extends MySQLDAO implements GameDAO{
@@ -25,22 +29,77 @@ public class MySQLGameDAO extends MySQLDAO implements GameDAO{
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        if (gameID == 0) {
+            throw new DataAccessException("Game does not exist");
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            String preparedStatement = "SELECT * FROM games WHERE gameID=?;";
+            try (var ps = conn.prepareStatement(preparedStatement)) {
+                ps.setInt(1, gameID);
+                ResultSet rs = ps.executeQuery();
+                rs.next();
+                //deserialize game json
+                ChessGame game = new Gson().fromJson(rs.getString("game"), ChessGame.class);
+                return new GameData(rs.getInt("gameID"), rs.getString("whiteUsername"),
+                        rs.getString("blackUsername"), rs.getString("gameName"), game);
+            }
+        } catch (DataAccessException | SQLException e) {
+            throw new DataAccessException("Game does not exist");
+        }
     }
 
     public int createGame(String gameName) {
-        return 0;
+        if (gameName == null) {
+            return 0;
+        }
+        String preparedStatement = "INSERT INTO games (gameName, game) VALUES (?, ?)";
+        var game = new ChessGame();
+        var gameJson = new Gson().toJson(game);
+        try {
+            return executeUpdate(preparedStatement, gameName, gameJson);
+        } catch (DataAccessException e) {
+            return 0;
+        }
     }
 
     public Collection<GameData> listGames() {
-        return null;
+        Collection<GameData> games = new ArrayList<GameData>();
+        try (var conn = DatabaseManager.getConnection()) {
+            String preparedStatement = "SELECT * FROM games";
+            try (var ps = conn.prepareStatement(preparedStatement)) {
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    ChessGame game = new Gson().fromJson(rs.getString("game"), ChessGame.class);
+                    games.add(new GameData(rs.getInt("gameID"), rs.getString("whiteUsername"),
+                            rs.getString("blackUsername"), rs.getString("gameName"), game));
+                }
+                return games;
+            }
+        } catch (DataAccessException | SQLException e) {
+            return null;
+        }
     }
 
     public void updateGame(GameData gameData, GameData newData) throws DataAccessException {
-
+        //delete old game data
+        String preparedStatement = "DELETE FROM games WHERE gameID=?;";
+        executeUpdate(preparedStatement, gameData.gameID());
+        //add new game data
+        String game = new Gson().toJson(newData.game());
+        preparedStatement = """
+                            INSERT INTO games (gameID, whiteUsername, blackUsername, gameName, game) 
+                            VALUES (?, ?, ?, ?, ?)
+                            """;
+        executeUpdate(preparedStatement, newData.gameID(), newData.whiteUsername(), newData.blackUsername(),
+                newData.gameName(), game);
     }
 
     public void deleteGames() {
-
+        String preparedStatement = "TRUNCATE games;";
+        try {
+            executeUpdate(preparedStatement);
+        } catch (DataAccessException e) {
+            return;
+        }
     }
 }
--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,10 +1,13 @@
 package dataaccess;
 
+import chess.ChessGame;
 import dataaccess.mysql.*;
 import model.*;
 
 import org.junit.jupiter.api.*;
 import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.ArrayList;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DataAccessTests {
@@ -14,6 +17,7 @@ public class DataAccessTests {
     private static MySQLAuthDAO authDAO;
 
     private static String currentAuthToken;
+    private static int currentGameID;
 
     @BeforeAll
     public static void init() {
@@ -28,6 +32,13 @@ public class DataAccessTests {
         catch (DataAccessException e) {
             System.out.printf("Unable to initialize database: %s", e.getMessage());
         }
+    }
+
+    @AfterAll
+    public static void clearDatabase() {
+        userDAO.deleteUsers();
+        authDAO.deleteAuths();
+        gameDAO.deleteGames();
     }
 
     /*
@@ -49,7 +60,8 @@ public class DataAccessTests {
     @Order(2)
     public void createUserNegative() {
         //try to create already existing user
-        Assertions.assertThrows(DataAccessException.class, () -> userDAO.createUser(new UserData("johndoe", "12345", "johndoe@email.com")));
+        Assertions.assertThrows(DataAccessException.class, () -> userDAO.createUser(new UserData("johndoe",
+                "12345", "johndoe@email.com")));
     }
 
     @Test
@@ -129,8 +141,104 @@ public class DataAccessTests {
     @Test
     @Order(11)
     public void clearAuths() {
-        userDAO.deleteUsers();
         authDAO.deleteAuths();
         Assertions.assertThrows(DataAccessException.class, () -> authDAO.getAuth(currentAuthToken));
+    }
+
+    /*
+        GAME TESTS
+     */
+    @Test
+    @Order(12)
+    public void createGamePositive() {
+        currentGameID = gameDAO.createGame("john's game");
+        getGamePositive();
+    }
+
+    @Test
+    @Order(13)
+    public void createGameNegative() {
+        //attempt to create a game with a null name
+        int gameID = gameDAO.createGame(null);
+        Assertions.assertThrows(DataAccessException.class, () -> gameDAO.getGame(gameID));
+    }
+
+    @Test
+    @Order(14)
+    public void getGamePositive() {
+        try {
+            GameData actualGame = gameDAO.getGame(currentGameID);
+            GameData expectedGame = new GameData(currentGameID, null, null,
+                    "john's game", new ChessGame());
+            Assertions.assertEquals(expectedGame, actualGame);
+        } catch (DataAccessException e) {
+            return;
+        }
+    }
+
+    @Test
+    @Order(15)
+    public void getGameNegative() {
+        //attempt to get a non-existing game
+        Assertions.assertThrows(DataAccessException.class, () -> gameDAO.getGame(-1));
+    }
+
+    @Test
+    @Order(16)
+    public void listGamesPositive() {
+        //add another game
+        gameDAO.createGame("doe's game");
+        ArrayList<GameData> actualGames = (ArrayList<GameData>) gameDAO.listGames();
+        var expectedGames = new ArrayList<>(){{
+            add(new GameData(1, null, null,
+                "john's game", new ChessGame()));
+            add(new GameData(2, null, null,
+                    "doe's game", new ChessGame()));}};
+        Assertions.assertEquals(expectedGames, actualGames);
+    }
+
+    @Test
+    @Order(17)
+    public void listGamesNegative() {
+        //try to list games when database is empty
+        gameDAO.deleteGames();
+        var games = gameDAO.listGames();
+        Assertions.assertEquals(new ArrayList<>(), games);
+    }
+
+    @Test
+    @Order(18)
+    public void updateGamePositive() {
+        currentGameID = gameDAO.createGame("john's game");
+        try {
+            GameData currentGame = gameDAO.getGame(currentGameID);
+            GameData updatedGame = new GameData(currentGame.gameID(), "john", "doe",
+                    currentGame.gameName(), currentGame.game());
+            gameDAO.updateGame(currentGame, updatedGame);
+        } catch (DataAccessException e) {
+            return;
+        }
+    }
+
+    @Test
+    @Order(19)
+    public void updateGameNegative() {
+        //attempt to update a game with a null game name
+        try {
+            GameData currentGame = gameDAO.getGame(currentGameID);
+            GameData updatedGame = new GameData(currentGame.gameID(), "john", "doe",
+                    null, currentGame.game());
+            Assertions.assertThrows(DataAccessException.class, () -> gameDAO.updateGame(currentGame, updatedGame));
+        } catch (DataAccessException e) {
+            return;
+        }
+
+    }
+
+    @Test
+    @Order(20)
+    public void clearGames() {
+        gameDAO.deleteGames();
+        Assertions.assertThrows(DataAccessException.class, () -> gameDAO.getGame(1));
     }
 }
