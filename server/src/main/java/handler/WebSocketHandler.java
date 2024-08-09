42d4fa9283232e6d669f6dfcb2d9e19b386b4ca9
porterweston <porterwestonbiz@gmail.com>
2024-08-09 12:06:19 -0600

    WebSocketSessions implemented

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -1,4 +1,47 @@
 package handler;
 
+import org.eclipse.jetty.websocket.api.annotations.*;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.*;
+
+@WebSocket
 public class WebSocketHandler {
+    private final WebSocketSessions sessions = new WebSocketSessions();
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {
+
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session session) {
+
+    }
+
+    @OnWebSocketError
+    public void onError(Throwable throwable) {
+
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String str) {
+        //determine message type
+
+    }
+
+    private void connect(UserGameCommand command) {
+
+    }
+
+    private void makeMove(MakeMoveCommand command) {
+
+    }
+
+    private void leaveGame(UserGameCommand command) {
+
+    }
+
+    private void resignGame(UserGameCommand command) {
+
+    }
 }
---
+++ server/src/main/java/handler/WebSocketSessions.java
@@ -0,0 +1,39 @@
+package handler;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class WebSocketSessions {
+    private final Map<Integer, Set<Session>> sessionMap = new HashMap<>();
+
+    public void addSessionToGame(Integer gameID, Session session) {
+        sessionMap.computeIfAbsent(gameID, k -> new HashSet<Session>());
+        Set<Session> sessionSet = sessionMap.get(gameID);
+        sessionSet.add(session);
+        sessionMap.put(gameID, sessionSet);
+    }
+
+    public void removeSessionFromGame(Integer gameID, Session session) {
+        if (!(sessionMap.get(gameID) == null)) {
+            Set<Session> sessionSet = sessionMap.get(gameID);
+            sessionSet.remove(session);
+            sessionMap.put(gameID, sessionSet);
+        }
+    }
+
+    public void removeSession(Session session) {
+        for (Integer gameID : sessionMap.keySet()) {
+            Set<Session> sessionSet = sessionMap.get(gameID);
+            sessionSet.remove(session);
+            sessionMap.put(gameID, sessionSet);
+        }
+    }
+
+    public Set<Session> getSessionsForGame(Integer gameID) {
+        return sessionMap.get(gameID);
+    }
+}
