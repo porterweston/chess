d7bc2dab8c96fc5aeb83751a8865a5e56f6a7d44
porterweston <porterwestonbiz@gmail.com>
2024-08-10 14:41:13 -0600

    WebSocketService connect initial implementation

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -1,12 +1,16 @@
 package handler;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.commands.*;
+import service.*;
+import websocket.messages.*;
 
 @WebSocket
 public class WebSocketHandler {
     private final WebSocketSessions sessions = new WebSocketSessions();
+    private final WebSocketService service = new WebSocketService();
 
     @OnWebSocketConnect
     public void onConnect(Session session) {
@@ -24,24 +28,53 @@ public class WebSocketHandler {
     }
 
     @OnWebSocketMessage
-    public void onMessage(Session session, String str) {
-        //determine message type
+    public void onMessage(Session session, String command) throws ErrorException{
+        //deserialize command
+        UserGameCommand c = new Gson().fromJson(command, UserGameCommand.class);
+        //determine command type
+        switch (c.getCommandType()) {
+            case UserGameCommand.CommandType.CONNECT -> connect(session, c);
+            case UserGameCommand.CommandType.MAKE_MOVE -> makeMove(session, (MakeMoveCommand)c);
+            case UserGameCommand.CommandType.LEAVE -> leaveGame(session, c);
+            case UserGameCommand.CommandType.RESIGN -> resignGame(session, c);
+        }
+    }
+
+    private void connect(Session session, UserGameCommand command) throws ErrorException{
+        sessions.addSessionToGame(command.getGameID(), session);
+        var messages = service.connect(command.getAuthToken(), command.getGameID());
+        sendMessage(messages[0], session);
+        sendBroadcast(command.getGameID(), messages[1], session);
+    }
+
+    private void makeMove(Session session, MakeMoveCommand command) throws ErrorException{
+        var messages = service.makeMove(command.getAuthToken(), command.getGameID(), command.getMove());
+        sendBroadcast(command.getGameID(), messages[0], null);
+        sendBroadcast(command.getGameID(), messages[1], session);
+        //move resulted in check/checkmate/stalemate
+        if (messages[2] != null) {
+            sendBroadcast(command.getGameID(), messages[2], null);
+        }
+    }
+
+    private void leaveGame(Session session, UserGameCommand command) throws ErrorException{
+        sessions.removeSessionFromGame(command.getGameID(), session);
+        var message = service.leaveGame(command.getAuthToken(), command.getGameID());
+        sendBroadcast(command.getGameID(), message, session);
+    }
+
+    private void resignGame(Session session, UserGameCommand command) throws ErrorException{
+        var message = service.resignGame(command.getAuthToken(), command.getGameID());
+        sendBroadcast(command.getGameID(), message, null);
+    }
+
+    //sends a message to a given session
+    private void sendMessage(ServerMessage message, Session session) {
 
     }
 
-    private void connect(UserGameCommand command) {
-
-    }
-
-    private void makeMove(MakeMoveCommand command) {
-
-    }
-
-    private void leaveGame(UserGameCommand command) {
-
-    }
-
-    private void resignGame(UserGameCommand command) {
+    //sends a message to all sessions excluding the given session
+    private void sendBroadcast(Integer gameID, ServerMessage message, Session exceptThisSession) {
 
     }
 }
--- server/src/main/java/service/WebSocketService.java
+++ server/src/main/java/service/WebSocketService.java
@@ -1,4 +1,61 @@
 package service;
 
+import chess.*;
+import dataaccess.mysql.MySQLAuthDAO;
+import websocket.messages.*;
+import dataaccess.*;
+import dataaccess.mysql.*;
+import model.*;
+
 public class WebSocketService {
+    private static MySQLGameDAO gameDAO;
+    private static MySQLAuthDAO authDAO;
+
+    public WebSocketService() {
+        try {
+            gameDAO = new MySQLGameDAO();
+            authDAO = new MySQLAuthDAO();
+        } catch (DataAccessException e) {
+            System.out.printf("Unable to initialize database: %s", e.getMessage());
+        }
+    }
+
+    public ServerMessage[] connect(String authToken, Integer gameID) throws ErrorException{
+        try {
+            GameData gameData = gameDAO.getGame(gameID);
+            LoadGameMessage loadMessage = new LoadGameMessage(gameData.game());
+
+            String username = authDAO.getAuth(authToken).username();
+            //access the color that the user joined as
+            String whiteUser = gameData.whiteUsername();
+            String blackUser = gameData.blackUsername();
+            NotificationMessage notificationMessage;
+            if (username.equals(whiteUser)) {
+                notificationMessage = new NotificationMessage(String.format("%s has joined the game as white", username));
+            }
+            else if (username.equals(blackUser)) {
+                notificationMessage = new NotificationMessage(String.format("%s has joined the game as black", username));
+            }
+            else {
+                notificationMessage = new NotificationMessage(String.format("%s has joined the game as an observer", username));
+            }
+
+            return new ServerMessage[]{loadMessage, notificationMessage};
+        } catch (DataAccessException e) {
+            throw new ErrorException(400, e.getMessage());
+        }
+
+    }
+
+    public ServerMessage[] makeMove(String authToken, Integer gameID, ChessMove move) throws ErrorException{
+        return null;
+    }
+
+    public ServerMessage leaveGame(String authToken, Integer gameID) throws ErrorException{
+        return new NotificationMessage("message");
+    }
+
+    public ServerMessage resignGame(String authToken, Integer gameID) throws ErrorException{
+        return null;
+    }
 }
